2a90204a2bf7d5ad05b2b734c4e8debc18a67848
unknown <myadiuio@gmail.com>
2021-01-04 21:04:49 +0900

    Update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ class musicInfo {
     }
 }
 
-exports.searchSong = async ({title: title, artist: artist, album: album }) => {
+exports.searchSong = async ({ title: title, artist: artist, album: album }) => {
 
     if(!title) throw new TypeError("missing parameter: title")
 
@@ -27,6 +27,33 @@ exports.searchSong = async ({title: title, artist: artist, album: album }) => {
         discNumber: res.results[0].discNumber,
         trackNumber: res.results[0].trackNumber,
         explicit: res.results[0].trackExplicitness == "notExplicit" ? false : true,
+        releaseDate: res.results[0].releaseDate,
+        genre: res.results[0].primaryGenreName,
+        country: res.results[0].country
+    }
+
+    return result
+
+}
+
+exports.searchAlbum = async ({ name: name, artist: artist }) => {
+
+    if(!name) throw new TypeError("missing parameter: name")
+
+    artist = typeof artist !== 'undefined' ? `+${artist}` : "";
+
+    let searchData = await fetch(encodeURI(`https://itunes.apple.com/search?term=${name}${artist}&limit=1&entity=album`))
+    let res = await searchData.json()
+    if (res.resultCount == 0) {
+        throw new Error("No result.")
+    }
+
+    let result = {
+        name: res.results[0].collectionName,
+        artist: res.results[0].artistName,
+        trackCount: res.results[0].trackCount,
+        explicit: res.results[0].collectionExplicitness == "notExplicit" ? false : true,
+        contentAdvisoryRating: res.results[0].contentAdvisoryRating ? res.results[0].contentAdvisoryRating : null,
         releaseDate: res.results[0].releaseDate,
         genre: res.results[0].primaryGenreName,
         country: res.results[0].country
